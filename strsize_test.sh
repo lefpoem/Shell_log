2d853f2a434042a7bc586144b7af83dad7a6ce6d
lem <73171983+lefpoem@users.noreply.github.com>
2021-05-29 19:26:58 +0800

    Update strsize_test.sh

--- strsize_test.sh
+++ strsize_test.sh
@@ -1,19 +1,19 @@
 #! /bin/bash
 a="abc"
 b="def"
-if [ -z $a ]
+if [ -z $a ]//-z 判断字符串长度是否为0，为0返回true
 then
     echo "the size fo string a is 0"
 else
     echo "the size isn't 0"
 fi
-if [ -n $a ]
+if [ -n $a ]//-n 判断字符串长度是否为0，为0返回false
 then
     echo "the size of string a is not 0"
 else
     echo "the size is 0"
 fi
-if [ $a ]
+if [ $a ]//$a 判断字符串长度是否为0，为0返回true
 then
     echo "the size is not 0"
 else
