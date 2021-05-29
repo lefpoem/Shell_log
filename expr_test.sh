7c763e7314463ba5154261921169bde74fce9dcb
lem <73171983+lefpoem@users.noreply.github.com>
2021-05-29 12:03:27 +0800

    Update expr_test.sh

--- expr_test.sh
+++ expr_test.sh
@@ -5,13 +5,13 @@ val=`expr $a + $b`
 echo "a + b: $val"
 val=`expr $a - $b`
 echo "a - b: $val"
-val=`expr $a \* $b`
+val=`expr $a \* $b`//乘号前必须加空格
 echo "a * b: $val"
 val=`expr $b / $a`
 echo "b / a: $val"
 val=`expr $b % $a`
 echo "b % a: $val"
-if [ $a == $b ]
+if [ $a == $b ]   //表达式中必须有空格
 then
    echo "a is equal to b"
 fi
